9d863c8454aad23a92ebe77070e40ab1cdffd488
MichaelBamba <mjbamba2@gmail.com>
2020-02-10 14:46:49 -0500

    completed chuckapi project

--- jav/chuckjav.js
+++ jav/chuckjav.js
@@ -1,7 +1,21 @@
 `use strict`
 const apiurl = `https://api.chucknorris.io/jokes/random?category=dev`;
+const Quotebutton = document.querySelector('#refreshQuote');
 
-(function () {
+function getQuote() {
     const chuckSaysParagraph = document.querySelector('#chuckSays');
-    chuckSaysParagraph.innerHTML = 'chuck in an iife'
-})();
+    chuckSaysParagraph.innerHTML = 'chuck in an iife';
+    get(apiurl)
+        .then(function (response) {
+            chuckSaysParagraph.innerHTML = response.value;
+            //     console.log(response.value);
+        }
+        );
+};
+
+
+Quotebutton.addEventListener('click', function (e) {
+    e.preventDefault();
+    getQuote();
+});
+getQuote();
