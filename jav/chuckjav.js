f151f514be485122f66bd5378ab378fd6f25214e
MichaelBamba <mjbamba2@gmail.com>
2020-02-10 13:58:05 -0500

    first commit stuff

---
+++ jav/chuckjav.js
@@ -0,0 +1,7 @@
+`use strict`
+const apiurl = `https://api.chucknorris.io/jokes/random?category=dev`;
+
+(function () {
+    const chuckSaysParagraph = document.querySelector('#chuckSays');
+    chuckSaysParagraph.innerHTML = 'chuck in an iife'
+})();
